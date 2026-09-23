7de57fabb66b72b25bd90d03c8cd5f867b9344c1
karthi <karthikeyanr9a@gmail.com>
2026-09-23 15:47:19 +0530

    Added data transformation process

--- e-commerce.py
+++ e-commerce.py
@@ -47,3 +47,36 @@ print("\nInvalid Prices:", np.sum(df["Price"] < 0))
 print("\nInvalid Quantities:", np.sum(df["Quantity"] <= 0))
 
 df.to_csv("cleaned_ecommerce_data.csv", index=False)
+
+df = pd.read_csv("cleaned_ecommerce_data.csv")
+print("\nDATA TRANSFORMATION PROCESS")
+df["Total Sales"] = df["Quantity"] * df["Price"]
+df["Discount %"] = np.where(df["Total Sales"] >= 50000, 10,np.where(df["Total Sales"] >= 20000, 5, 0))
+df["Discount Amount"] = df["Total Sales"] * df["Discount %"] / 100
+df["Net Sales"] = df["Total Sales"] - df["Discount Amount"]
+df["Profit"] = df["Net Sales"] * 0.10
+df["Profit Margin"] = df["Profit"] / df["Net Sales"] * 100
+
+print("\n",df[[
+    "Order_ID",
+    "Quantity",
+    "Price",
+    "Total Sales",
+    "Discount %",
+    "Discount Amount",
+    "Net Sales",
+    "Profit",
+    "Profit Margin"
+]].head())
+print(df[[
+    "Total Sales",
+    "Discount %",
+    "Discount Amount",
+    "Net Sales",
+    "Profit",
+    "Profit Margin"
+]].isnull().sum())
+print((df["Net Sales"] < 0).sum())
+print(df["Discount %"].unique())
+df.to_csv("transformed-ecommerce-data.csv", index=False)
+print("\nTransformed dataset saved successfully.")
